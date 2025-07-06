07dfbeb1214112f0a12d71986d7fa68cafa5e888
ivanro2872 <ivanenkov28111972@gmail.com>
2025-07-06 12:57:49 +0300

    Add files via upload

---
+++ queries.sql
@@ -0,0 +1,7 @@
+SELECT COUNT(*) AS customers_count
+FROM customers;
+/*
+Считаем всех покупателей из таблицы customers.
+COUNT(*) — агрегатная функция, подсчитывает все строки в таблице.
+AS customers_count — дает имя для столбца.
+*/

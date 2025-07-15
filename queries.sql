83b5168347f0b74e9494a82db6fcb740df23bf5f
ivanro2872 <ivanenkov28111972@gmail.com>
2025-07-15 22:51:14 +0300

    Delete queries.sql

--- queries.sql
+++
@@ -1,7 +0,0 @@
-SELECT COUNT(*) AS customers_count
-FROM customers;
-/*
-Считаем всех покупателей из таблицы customers.
-COUNT(*) — агрегатная функция, подсчитывает все строки в таблице.
-AS customers_count — дает имя для столбца.
-*/

9112bb41dbab4977cca5807e9dc44d0a797b2431
ivanro2872 <ivanenkov28111972@gmail.com>
2025-08-11 21:54:47 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -39,14 +39,14 @@ ORDER BY average_income ASC;
 -- отчет по выручке по каждому продавцу и дню недели
 SELECT
     e.first_name || ' ' || e.last_name AS seller,
-    LOWER(TRIM(TO_CHAR(s.sale_date, 'day'))) AS day_of_week,
+    TO_CHAR(s.sale_date, 'day') AS day_of_week,
     FLOOR(SUM(p.price * s.quantity)) AS income
 FROM sales AS s
 INNER JOIN employees AS e ON s.sales_person_id = e.employee_id
 INNER JOIN products AS p ON s.product_id = p.product_id
 GROUP BY e.first_name, e.last_name, s.sale_date
 ORDER BY
-    EXTRACT(DOW FROM s.sale_date),
+    EXTRACT(ISODOW FROM s.sale_date),
     e.first_name || ' ' || e.last_name;
 
 -- выбираем возрастные группы и количество покупателей в каждой группе
@@ -65,14 +65,7 @@ GROUP BY
         WHEN age BETWEEN 26 AND 40 THEN '26-40'
         ELSE '40+'
     END
-ORDER BY
-    MIN(
-        CASE
-            WHEN age BETWEEN 16 AND 25 THEN 1
-            WHEN age BETWEEN 26 AND 40 THEN 2
-            ELSE 3
-        END
-    );
+ORDER BY age_category;
 
 -- выбираем данные по месяцам - год и месяц из даты продажи
 SELECT
@@ -81,7 +74,6 @@ SELECT
     FLOOR(SUM(s.quantity * p.price)) AS income
 FROM sales AS s
 INNER JOIN products AS p ON s.product_id = p.product_id
-WHERE s.sale_date IS NOT NULL
 GROUP BY TO_CHAR(s.sale_date, 'YYYY-MM')
 ORDER BY selling_month;
 
@@ -99,21 +91,16 @@ WITH first_sales AS (
     FROM sales AS s
     INNER JOIN products AS p ON s.product_id = p.product_id
     WHERE p.price = 0
-),
-
-first_free_customers AS (
-    SELECT *
-    FROM first_sales
-    WHERE rn = 1
 )
 
 SELECT
-    fpc.sale_date,
+    fs.sale_date,
     c.first_name || ' ' || c.last_name AS customer,
     e.first_name || ' ' || e.last_name AS seller
-FROM first_free_customers AS fpc
-INNER JOIN customers AS c ON fpc.customer_id = c.customer_id
-INNER JOIN employees AS e ON fpc.employee_id = e.employee_id
+FROM first_sales AS fs
+INNER JOIN customers AS c ON fs.customer_id = c.customer_id
+INNER JOIN employees AS e ON fs.employee_id = e.employee_id
+WHERE fs.rn = 1
 ORDER BY c.customer_id;
 
 
